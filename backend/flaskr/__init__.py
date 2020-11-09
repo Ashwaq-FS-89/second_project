897c48cb85a58d06f309094e535df43af51ceee5
ashwaq <>
2020-11-10 00:30:57 +0300

    first commit

---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,197 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+
+from models import setup_db, Question, Category
+
+
+
+def create_app(test_config=None):
+ 
+  app = Flask(__name__)
+  setup_db(app)
+  
+  
+  CORS(app, resources={r"/api/*": {"origins": "*"}})
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
+  
+  @app.route('/categories')
+  def get_categories():
+    category=Category.query.all()
+    data={}
+    for i in category:
+      print(i)
+      data[i.id]=i.type
+    
+    return jsonify({
+          'success': True,
+          'categories':data
+        })
+
+
+  @app.route('/questions',methods=['POST'])
+  def post_question():
+    data=request.get_json()
+    new_question= Question(question=data['question'],
+      answer=data['answer'],
+      difficulty=data['difficulty'],
+      category=data['category'])
+    new_question.insert()
+    
+    print(data)
+    return jsonify({
+          'success': True,
+          'questions':new_question
+        })
+
+      
+    
+
+  
+  @app.route('/questions')
+  def get_question():
+    questions  = Question.query.all()
+    page = request.args.get('page', 1, type=int)
+    limit= request.args.get('limit', 10, type=int)
+    offset=(page-1) * limit
+    end=offset + limit
+    data=[qu.format()for qu in questions]
+    
+
+    qustion_paginate= data[offset:end]
+    print(data)
+
+    category=Category.query.all()
+    data1={}
+    for i in category:
+      data1[i.id]=i.type
+    
+
+    return jsonify({
+          'success': True,
+          'questions': qustion_paginate,
+          'totalQuestions':len(questions),
+          'categories': data1,
+          'currentCategory': None
+           
+        })
+
+      
+  @app.route('/questions/<int:id>', methods=['DELETE'])
+  def delete_question(id):
+    question = Question.query.get(id)
+    question.delete()
+    
+
+    return jsonify({
+    'success': True,
+    'delated':id
+        })
+
+  @app.route('/questions/search', methods=['POST'])
+  def search_question():  
+    data=request.get_json()
+    search_term=data['searchTerm']
+    result =Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
+    page = request.args.get('page', 1, type=int)
+    limit= request.args.get('limit', 10, type=int)
+    offset=(page-1) * limit
+    end=offset + limit
+    data=[qu.format()for qu in result]
+    qustion_paginate= data[offset:end]
+   
+    return jsonify({
+            'success': True,
+            'questions': qustion_paginate,
+            'totalQuestions':len(result),
+            'currentCategory':None
+            
+          })
+    
+ 
+
+  
+  
+  @app.route('/categories/<int:category_id>/questions')
+  def get_questions_by_categories(category_id):
+    question=Question.query.filter_by(category=category_id).all()
+    page = request.args.get('page', 1, type=int)
+    limit= request.args.get('limit', 10, type=int)
+    offset=(page-1) * limit
+    end=offset + limit
+    data=[qu.format()for qu in question]
+    qustion_paginate= data[offset:end]
+    return jsonify({
+          'success': True,
+          'questions': qustion_paginate,
+          'total_questions': len(question),
+          'current_category':category_id
+        })
+
+  @app.route('/quizzes', methods=['POST'])
+  def play_quizzes(): 
+    data=request.get_json()
+    previous_questions= data['previous_questions']
+    category= data['quiz_category']
+
+    question=Question.query.filter(Question.category==category).all()
+
+    new_question=random.choice(question)
+
+    return jsonify({
+          'success': True,
+          'previousQuestions':previous_questions,
+          'currentQuestion':new_question
+        })
+
+ 
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 404,
+        "message": "Not found"
+        }), 404
+
+  @app.errorhandler(405)
+  def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 405,
+        "message": "Method Not Allowed"
+        }), 405
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False, 
+      "error": 422,
+      "message": "unprocessable"
+      }), 422
+
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+        "success": False, 
+        "error": 400,
+        "message": "Bad Request"
+        }), 400
+        
+  @app.errorhandler(500)
+  def internal_server_error(error):
+    return jsonify({
+        "success": False, 
+        "error": 500,
+        "message": " Internal Server Error"
+        }), 500
+  return app
+
+    
---
+++ backend/test_flaskr.py
@@ -0,0 +1,114 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+     
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertTrue(len(data['categories']))
+
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    
+    
+    def test_new_question(self):
+        new_question = {
+            'question':'what is faviorate subjuct',
+            'answer': 'cumputer',
+            'difficulty':2 ,
+            'category':1 }
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_new_question_fail(self):
+        new_question = {
+            'question':'what is faviorate subjuct',
+            'answer': 'cumputer',
+            'difficulty':2 ,
+            'category':1 
+            }
+        res = self.client().post('/categories', json= new_question)
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Method Not Allowed')
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 1)
+
+    def test_get_questions_by_categories(self):
+        res = self.client().get('/categories/3/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_search_question(self):
+        search_term =  {'searchTerm':'name'}
+        res = self.client().post('/questions/search', json=search_term)
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        
+        
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
